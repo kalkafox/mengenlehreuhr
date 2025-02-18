9a09e9a76ecd0a36a7476ea64d4b5eeb5f575d15
Kalka <kalka2088@gmail.com>
2025-02-18 06:08:43 -0500

    forgot about this bit

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
+if (process.env.GH_REPO) {
+  console.log('GitHub repo detected!')
+}
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.GH_REPO ?? '/',
 })
