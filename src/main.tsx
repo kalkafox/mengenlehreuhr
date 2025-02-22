084a4699854708cfcde96f8bc14cf5e623831608
Kalka <kalka2088@gmail.com>
2025-02-22 14:37:58 -0500

    this probably doesn't work

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import '@/lib/i18n'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import LanguageDetector from 'i18next-browser-languagedetector'
 import { Provider } from 'jotai'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
@@ -8,17 +7,9 @@ import App from './app.tsx'
 import { ThemeProvider } from './components/theme-provider.tsx'
 import { Toaster } from './components/ui/toaster.tsx'
 import './index.css'
-import { languageAtom, store } from './lib/atom.ts'
+import { store } from './lib/atom.ts'
 
 const queryClient = new QueryClient()
-
-const languageDetector = new LanguageDetector()
-
-const language = languageDetector.detect()
-
-if (language && !Array.isArray(language)) {
-  store.set(languageAtom, language)
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   const loading = document.getElementById('loading-bar')!
